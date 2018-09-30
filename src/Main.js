c8dba0562df6210abceb29a0aeea665defac60b5
anitah kimanzi <anitakimanzi@gmail.com>
2018-09-30 21:27:58 +0300

    show phone type

--- src/Main.js
+++ src/Main.js
@@ -8,33 +8,6 @@ import Signup from "./Signup";
 import Login from "./Login";
  
 
-// var isMobile = {
-//   Android: function() {
-//       return navigator.userAgent.match(/Android/i);
-//   },
-//   BlackBerry: function() {
-//       return navigator.userAgent.match(/BlackBerry/i);
-//   },
-//   iOS: function() {
-//       return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-//   },
-//   Opera: function() {
-//       return navigator.userAgent.match(/Opera Mini/i);
-//   },
-//   Windows: function() {
-//       return navigator.userAgent.match(/IEMobile/i);
-//   },
-//   any: function() {
-//       return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-//   }
-// };
-// if( isMobile.Window() ) alert('window');
-var uagent = navigator.userAgent.toLowerCase();
-       if (uagent.search("chrome") > -1)
-          alert('true');
-       else
-          alert('false');
-
 
 class Main extends Component {
   render() {
