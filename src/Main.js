11112b0c649cfd81c3fd598e3300d5184acd3963
anitah kimanzi <anitakimanzi@gmail.com>
2018-10-01 14:04:28 +0300

    added alert in opera

--- src/Main.js
+++ src/Main.js
@@ -10,20 +10,31 @@ import Login from "./Login";
 
 // let isChrome = !!window.chrome && !!window.chrome.webstore; 
 
+var isMobile = {
+  Android: function() {
+      return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+      return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+  }
+};
+
+
+if( isMobile.Opera() ) alert('opera');
+
 class Main extends Component {
-
-
-  // content(){
-  //   if(isChrome){
-  //       return (
-  //           <div>Chrome</div>
-  //       )
-  //   } else {
-  //       return (
-  //           <div>Not Chrome</div>
-  //       )
-  //   }
-  // }
 
   render() {
     return (
